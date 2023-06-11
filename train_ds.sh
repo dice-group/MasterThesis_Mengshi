b9049e4f1ca929e5e552f428ff7f64ab013b3e59
Mengshi Ma <makevin23@gmail.com>
2023-06-11 22:57:53 +0200

    train script for all languages

--- train_ds.sh
+++ train_ds.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-dataset_name=""
-model_name="google/mt5-xl"
+dataset_name="qald9-plus-all"
+model_name="fine-tuned_models/LC-QuAD"
 output_dir="fine-tuned_models/${dataset_name}"
-run_name="${model_name}-${dataset_name}"
-train_file=""
+run_name="LC-QuAD-${dataset_name}"
+train_file="datasets/q9pp_all_lang.csv"
 validation_file=""
 
 deepspeed code/train_new.py \
     --deepspeed deepspeed/ds_config_zero3.json \
     --model_name_or_path ${model_name} \
     --do_train \
-    --do_eval \
-    --eval_steps 1500 \
     --train_file ${train_file} \
-    --validation_file ${validation_file} \
     --output_dir ${output_dir} \
     --num_train_epochs 15 \
     --per_device_train_batch_size=4 \
