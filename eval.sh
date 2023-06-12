d7cec6ce74a948e15fd2429d2ce2c1e30c645525
Mengshi Ma <makevin23@gmail.com>
2023-06-12 16:44:16 +0200

    disable linguistic context

--- eval.sh
+++ eval.sh
@@ -3,7 +3,7 @@
 model="qald9-plus-all"
 exp_setting="LC-QuAD-all_lang"
 pred_pfad=pred_files/${model}/
-linguitic_context="False"
+linguitic_context="false"
 
 
 languages=("en" "de" "ru" "fr" "zh" "lt" "ja" "ba" "be" "uk" "es")
