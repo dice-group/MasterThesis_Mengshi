207eec551ae0636c30d9ff52102488b9fc7877d4
Mengshi Ma <makevin23@gmail.com>
2023-06-16 11:17:05 +0200

    evaluate checkpoints

--- eval.sh
+++ eval.sh
@@ -4,18 +4,19 @@ model="qald9-plus-all"
 exp_setting="LC-QuAD-all_lang"
 pred_pfad=pred_files/${model}/
 linguitic_context="false"
+steps=900
 
 
-languages=("en" "de" "ru" "fr" "zh" "lt" "ja" "ba" "be" "uk" "es")
+languages=("en" "de" "ru" "fr" "zh" "lt" "ja" "ba" "be" "uk")
 
 
 for lang in "${languages[@]}"
 do
     echo "Generating predicted qald file for ${lang}"
     python code/test_and_build_qald.py \
-        --model fine-tuned_models/${model} \
+        --model fine-tuned_models/${model}/checkpoint-${steps} \
         -t datasets/qald9plus/qald_9_pp_test_wikidata_linguistic.json \
-        -o pred_files/${model}/${lang}.json \
+        -o pred_files/${model}-${steps}/${lang}.json \
         -l ${lang}
 done
 
