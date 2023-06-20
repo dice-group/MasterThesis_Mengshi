a4ff036f5bdaeb3017adf40a216337dc29621947
Mengshi Ma <makevin23@gmail.com>
2023-06-20 12:40:00 +0200

    update train bash script

--- train_ds.sh
+++ train_ds.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-dataset_name=""
+dataset_name="lcquad-ling-entity"
 model_name="google/mt5-xl"
 output_dir="fine-tuned_models/${dataset_name}"
 run_name="${model_name}-${dataset_name}"
-train_file=""
-validation_file=""
+train_file="datasets/lcquad/train.csv"
 
 deepspeed --num_gpus=1 code/train_new.py \
     --deepspeed deepspeed/ds_config_zero3.json \
     --model_name_or_path ${model_name} \
     --do_train \
-    --do_eval \
-    --eval_steps 1500 \
     --train_file ${train_file} \
-    --validation_file ${validation_file} \
     --output_dir ${output_dir} \
     --num_train_epochs 15 \
     --per_device_train_batch_size=4 \
