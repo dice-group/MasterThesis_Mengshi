255c2c4e9ce426a95583aa27796db6dd5f063e4c
Mengshi Ma <makevin23@gmail.com>
2023-06-21 10:15:57 +0200

    refactor

--- eval.sh
+++ eval.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-model="lcquad-ling-entity"
-exp_setting="mt5-xl-lcquad-ling-entity"
-pred_path=pred_files/${exp_setting}
+model="q9pp-ling-context"
+exp_setting="all_lang_linguistic_mt5-xl"
+pred_path=pred_files/${model}/
+linguitic_context="True"
+
 
 languages=("en" "de" "ru" "fr" "zh" "lt" "ja" "ba" "be" "uk")
 
