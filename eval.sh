cf1e3ae0c10fcfe3838b4a80a464d5f817809714
Mengshi Ma <makevin23@gmail.com>
2023-06-21 15:52:12 +0200

    wip: script for eval

--- eval.sh
+++ eval.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-model="q9pp-ling-context"
-exp_setting="all_lang_linguistic_mt5-xl"
-pred_path=pred_files/${model}/
+model="mt5/12_mt5"
+exp_setting="mt5-base"
+pred_path=pred_files/${model}-new/
 linguitic_context="True"
 
 
