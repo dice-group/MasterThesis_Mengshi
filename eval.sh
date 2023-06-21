e198384e7415aa585d8ac0ddfd5331cdad700b75
Mengshi Ma <makevin23@gmail.com>
2023-06-21 16:27:37 +0200

    eval script

--- eval.sh
+++ eval.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-model="mt5/12_mt5"
+model="mt5/11_mt5"
 exp_setting="mt5-base"
-pred_path=pred_files/${model}-new/
+pred_path=pred_files/${model}/
 linguitic_context="True"
 
 
 languages=("en" "de" "ru" "fr" "zh" "lt" "ja" "ba" "be" "uk")
 
 
-for lang in "${languages[@]}"
-do
-    echo "Generating predicted qald file for ${lang}"
-    python code/test_and_build_qald.py \
-        --model fine-tuned_models/${model} \
-        -t datasets/qald9plus/qald_9_pp_test_wikidata_linguistic.json \
-        -o pred_files/${model}/${lang}.json \
-        -l ${lang}
-done
+# for lang in "${languages[@]}"
+# do
+#     echo "Generating predicted qald file for ${lang}"
+#     python code/test_and_build_qald.py \
+#         --model fine-tuned_models/${model} \
+#         -t datasets/qald9plus/qald_9_pp_test_wikidata_new.json \
+#         -o pred_files/${model}/${lang}.json \
+#         -l ${lang}
+# done
 
 echo "Start running GERBIL experiment"
 python code/gerbil_eval.py \
