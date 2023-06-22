9370b9a7e11d772e77cd44b02c98ca4e581f9e50
Mengshi Ma <makevin23@gmail.com>
2023-06-22 19:19:14 +0200

    add base script for pre-train

--- train_ds.sh
+++ train_ds.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-dataset_name=""
+dataset_name="LC-QuAD-ling-300rt"
 model_name="google/mt5-xl"
 output_dir="fine-tuned_models/${dataset_name}"
 run_name="${model_name}-${dataset_name}"
-train_file=""
-validation_file=""
+train_file="datasets/lcquad/lcquad_wikidata-ling.csv"
 
-deepspeed --num_gpus=1 code/train_new.py \
+deepspeed --include=localhost:0 --master_port 60000 code/train_new.py \
     --deepspeed deepspeed/ds_config_zero3.json \
     --model_name_or_path ${model_name} \
     --do_train \
-    --do_eval \
-    --eval_steps 1500 \
     --train_file ${train_file} \
-    --validation_file ${validation_file} \
     --output_dir ${output_dir} \
-    --num_train_epochs 15 \
+    --num_train_epochs 32 \
     --per_device_train_batch_size=16 \
     --overwrite_output_dir \
     --save_steps 3000 \
@@ -27,6 +23,7 @@ deepspeed --num_gpus=1 code/train_new.py \
     --fp16 0 \
     --gradient_checkpointing 1 \
     --gradient_accumulation_steps 4
+
 
     # for testing
     # --max_train_samples 100 \
