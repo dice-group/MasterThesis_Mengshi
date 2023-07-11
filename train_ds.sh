3ccaee6acb2e48ba3c8221c2135e2cb3fc5a1159
Mengshi Ma <makevin23@gmail.com>
2023-07-11 06:56:04 +0200

    script for fine-tune

--- train_ds.sh
+++ train_ds.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-run_name="pretrain-flant5-xl-lcquad2"
-model_name="google/flan-t5-xl"
+run_name="exp16-flant5-lcquad-lc-ek-pad"
+model_name="pre-trained_models/pretrain-flant5-xl-lcquad2"
 output_dir="pre-trained_models/${run_name}"
-train_file="datasets/lcquad2/train.csv"
+train_file="datasets/qald9plus/wikidata/qald_9_plus_train_wikidata.csv"
 
 deepspeed --include=localhost:1 --master_port 61000 code/train_new.py \
     --deepspeed deepspeed/ds_config_zero3.json \
