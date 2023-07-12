b83a5a1004e83a94776b123c102f5c3972091115
Mengshi Ma <makevin23@gmail.com>
2023-07-12 10:11:31 +0200

    update train script

--- train_ds.sh
+++ train_ds.sh
@@ -11,17 +11,17 @@ deepspeed --include=localhost:0 --master_port 60000 code/train_new.py \
     --do_train \
     --train_file ${train_file} \
     --output_dir ${output_dir} \
-    --num_train_epochs 15 \
+    --num_train_epochs 25 \
     --per_device_train_batch_size=16 \
     --overwrite_output_dir \
-    --save_steps 600 \
+    --save_steps 500 \
     --save_total_limit 2 \
     --report_to wandb \
     --run_name ${run_name}\
     --tf32 1 \
     --fp16 0 \
     --gradient_checkpointing 1 \
-    --gradient_accumulation_steps 4
+    --gradient_accumulation_steps 4 
 
     # for testing
     # --max_train_samples 100 \
